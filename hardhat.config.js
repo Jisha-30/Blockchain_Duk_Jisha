a51518127783203029de4f0c95b9b78fb8000432
Jisha-30 <jishas345@gmail.com>
2024-11-12 17:33:43 +0530

    deploy

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,15 +2,19 @@ require("@nomicfoundation/hardhat-toolbox");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  defaultNetwork: "localhost",
+  defaultNetwork: "sepolia",
   networks: {
     localhost: {
       url: "http://127.0.0.1:8545"
     },
     hardhat: {
       // See its defaults
-    }
-  },
+    },
+  sepolia: {
+    url: "https://eth-sepolia.g.alchemy.com/v2/y0nfh5uxuFB1L6zgDNj2cHy_C3D9Ntfp",
+    accounts: ["0b7c1988948e9b7fa4a52cea2887b09558c53bf675600a54df4b8d7b0033dfc1"]
+  }
+},
   solidity: {
     version: "0.8.20",
     settings: {
